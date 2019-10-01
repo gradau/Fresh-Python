8745cd54d61bd930811dc76cdde016fa2d25b9e0
gradau <george.radau@gmail.com>
2019-10-01 00:09:15 -0700

    fix typo

--- secretnum.py
+++ secretnum.py
@@ -8,6 +8,5 @@ secretNum = random.randint(1,100)
 
 for numGuesses in range(1,10):
     print('Take a guess!')
-    userGuess = int(intput()) #make users guess an int to be comparable to our secretnum
+    userGuess = int(input()) #make users guess an int to be comparable to our secretnum
 
-    
