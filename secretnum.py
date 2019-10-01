dac9ba8163b9d5db8558361d5c406cf812038477
gradau <george.radau@gmail.com>
2019-10-01 00:08:50 -0700

    start of guessing game

---
+++ secretnum.py
@@ -0,0 +1,13 @@
+#Number guessing game
+import random
+
+print('Welcome to the number guessing game!')
+print('I am thinking of a number between 1 and 100')
+
+secretNum = random.randint(1,100)
+
+for numGuesses in range(1,10):
+    print('Take a guess!')
+    userGuess = int(intput()) #make users guess an int to be comparable to our secretnum
+
+    
